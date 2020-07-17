fc22594d5abea74a7c5c7cf2cff0cb7e25037398
Rishabh Roshan Mallik <rishabhsun.71199@gmail.com>
2020-07-17 10:49:29 +0530

    modified DATABASE_URL

--- php/connectDB.php
+++ php/connectDB.php
@@ -2,7 +2,13 @@
 
 // $link = mysqli_connect("127.0.0.1:3325","root","","test");
 // $link = pg_connect("127.0.0.1:5432","postgres","R!shabh","fachione");
-$link = pg_connect("host=localhost dbname=fachione user=postgres password=R!shabh");
+// $link = pg_connect("host=localhost dbname=fachione user=postgres password=R!shabh");
+$link = (function(){
+    $parts = (parse_url(getenv('DATABASE_URL')));
+    extract($parts);
+    $path = ltrim($path, "/");
+    return new PDO("pgsql:host={$host};port={$port};dbname={$path}", $user, $pass);
+})();
 /* check connection */
 if (pg_last_error()) {
     printf("Connect failed: %s\n", pg_last_error());
